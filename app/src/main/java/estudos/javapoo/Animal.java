6f93b8284ddf236d537aa61edd7a5ae8cc0fcb07
Douglas Fonseca <douglasfonseca2010@hotmail.com>
2021-11-04 22:41:09 -0400

    Update Animal.java

--- app/src/main/java/estudos/javapoo/Animal.java
+++ app/src/main/java/estudos/javapoo/Animal.java
@@ -4,4 +4,12 @@ public class Animal {
     int tamanho;
     String cor;
     double peso;
+
+    void dormir(){
+        System.out.println("Dormir");
+    }
+
+    void correr(){
+        System.out.println("Correr");
+    }
 }
